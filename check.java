ea5fd7ffc62d1603ea65758e1b8b8f893c5ef041
Magie <107766055+Magiebalan@users.noreply.github.com>
2022-07-27 15:12:43 +0530

    Update check.java

--- check.java
+++ check.java
@@ -3,6 +3,7 @@ public class check{
 public static void main(String[] args){
 
 	System.out.println("jenkins to github trail is running");
-	System.out.println()"Checking _ 3 attempt";
+	System.out.println("Checking _ 3 attempt");
+	System.out.println("check 3");
 	}
 }
